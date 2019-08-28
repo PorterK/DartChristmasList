a017b4cbf88df8f03785300c04f9857fff097574
Kalob Porter <kalob@kalob.net>
2019-08-27 22:20:47 -0400

    Make the screen look a little better

--- lib/views/qr_scanner.dart
+++ lib/views/qr_scanner.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fast_qr_reader_view/fast_qr_reader_view.dart';
 
+import 'package:christmas_list/constants/theme.dart';
+
 class QrScanner extends StatefulWidget {
   QrScanner({ Key key }) : super(key: key);
 
@@ -19,6 +21,8 @@ class _QrScannerState extends State<StatefulWidget> {
   @override
   void dispose() {
     super.dispose();
+
+    controller?.dispose();
   }
 
   @override
@@ -37,22 +41,20 @@ class _QrScannerState extends State<StatefulWidget> {
       cameras[0],
       ResolutionPreset.high,
       [
-        CodeFormat.code128,
-        CodeFormat.code39,
-        CodeFormat.code93,
-        CodeFormat.datamatrix,
+        // CodeFormat.code128,
+        // CodeFormat.code39,
+        // CodeFormat.code93,
+        // CodeFormat.datamatrix,
         CodeFormat.ean13,
         CodeFormat.ean8,
-        CodeFormat.itf,
-        CodeFormat.pdf417,
+        // CodeFormat.itf,
+        // CodeFormat.pdf417,
         CodeFormat.qr,
         CodeFormat.upca,
         CodeFormat.upce
       ],
       (dynamic value) {
         print(value);
-
-        Future.delayed(const Duration(seconds: 3), controller.startScanning);
       }
     );
 
@@ -70,10 +72,45 @@ class _QrScannerState extends State<StatefulWidget> {
   @override
     Widget build(BuildContext context) {
       if (!canRender) {
-        return new Container();
+        return Container();
       }
-      return new AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: new QRReaderPreview(controller));
+      // return AspectRatio(
+      //   aspectRatio: 1,
+      //   child: QRReaderPreview(controller)
+      // );
+
+      return Scaffold(
+        backgroundColor: AppTheme.primary[500],
+        appBar: AppBar(
+          title: Text('Scan A Barcode'),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              // Container(
+              //   child: Column(
+              //     children: <Widget>[
+              //       Text('1. Point your camera at a barcode'),
+              //       Text('2. Point your camera at a barcode'),
+              //       Text('3. Point your camera at a barcode'),
+              //     ],
+              //   )
+              // ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / controller.value.aspectRatio,
+                child: Container(
+                  child: AspectRatio(
+                    aspectRatio: controller.value.aspectRatio,
+                    child: QRReaderPreview(controller),
+                  ),
+                )
+              )
+            ],
+          )
+        )
+      );
   }
 }
